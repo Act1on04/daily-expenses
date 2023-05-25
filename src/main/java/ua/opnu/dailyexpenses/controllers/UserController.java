fec4e2829f75cbfb31694a0fb968bf70b2b58dae
Alex Ishchenko <101579409+Act1on04@users.noreply.github.com>
2023-05-25 23:33:23 +0300

    update

--- src/main/java/ua/opnu/dailyexpenses/controllers/UserController.java
+++ src/main/java/ua/opnu/dailyexpenses/controllers/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ua.opnu.dailyexpenses.models.Expense;
 import ua.opnu.dailyexpenses.models.User;
 import ua.opnu.dailyexpenses.repositories.UserRepository;
 import ua.opnu.dailyexpenses.services.UserService;
@@ -50,21 +49,45 @@ public class UserController {
 
     }
 
-    @PostMapping("/login")
-    public String loginUser(@ModelAttribute("email") String email, @ModelAttribute("password") String password, BindingResult result, ModelMap model) {
-        System.out.println(email);
-        System.out.println(password);
-
-        if (!userRepo.existsByEmail(email)) {
-            result.rejectValue("email", "error.email","Обліковий запис з такою електронною поштою не існує");
-//        } else {
-//            if (!userRepo.findByEmail(email).getPassword().equals(password)) {
-//                result.rejectValue("password", "error.password", "Не відповідає пароль для облікового запису з такою електронною поштою");
-//            }
-        }
-        if (result.hasErrors()) return "/user/login";
-//        loggedUser = userRepo.findByEmail(email);
-        isLogged = true;
-        return "redirect:/expenses";
+//    @PostMapping("/login")
+//    public String loginUser(@ModelAttribute("email") String email,
+//                            BindingResult emailResult,
+//                            @ModelAttribute("password") String password,
+//                            BindingResult passwordResult,
+//                            ModelMap model) {
+//        System.out.println(email);
+//        System.out.println(password);
+//
+//        if (!userRepo.existsByEmail(email)) {
+//            emailResult.rejectValue("email", "error.email","Обліковий запис з такою електронною поштою не існує");
+////            String err = "Обліковий запис з такою електронною поштою не існує";
+////            ObjectError error = new ObjectError("globalError", err);
+////            emailResult.addError(error);
+////        } else {
+////            if (!userRepo.findByEmail(email).getPassword().equals(password)) {
+////                result.rejectValue("password", "error.password", "Не відповідає пароль для облікового запису з такою електронною поштою");
+////            }
+//        }
+//        if (emailResult.hasErrors()) return "/user/login";
+////        loggedUser = userRepo.findByEmail(email);
+//        isLogged = true;
+//        return "redirect:/expenses";
+//    }
+@PostMapping("/login")
+public String loginUser(User user, BindingResult result) {
+    String email = user.getEmail();
+    String password = user.getPassword();
+    if (!userRepo.existsByEmail(email)) {
+        result.rejectValue("email", "error.email","Обліковий запис з такою електронною поштою не існує");
+        } else {
+            if (!userRepo.findByEmail(email).getPassword().equals(password)) {
+                result.rejectValue("password", "error.password", "Не відповідає пароль для облікового запису з такою електронною поштою");
+            }
     }
+    if (result.hasErrors()) return "/user/login";
+    loggedUser = userRepo.findByEmail(email);
+    isLogged = true;
+    return "redirect:/expenses";
+}
+
 }
